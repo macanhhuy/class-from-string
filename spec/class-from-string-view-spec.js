a3c622e5a374e401d982f9060de08958b0780bff
Mac Anh Huy <macanhhuydn@gmail.com>
2016-10-13 10:43:08 +0700

    initial

---
+++ spec/class-from-string-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ClassFromStringView from '../lib/class-from-string-view';
+
+describe('ClassFromStringView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
